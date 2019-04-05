cd4e45ca7f44e8383f9d4243747bd484d7ab61ea
ychleo102615 <leo102615@gmail.com>
2019-04-05 22:27:17 +0800

    Nothing specially really, just keeping it a habbit to open this project

--- src/spinFrame.cpp
+++ src/spinFrame.cpp
@@ -24,7 +24,7 @@ Frame::Frame(ofVec3f center_, ofVec2f lv, int id_){
     
     initializePhysicalState();
     
-    CircularMove::setParameter(ofVec3f(1,1,0), 4, 1);
+    CircularMove::setParameter(ofVec3f(0,1,0), 4, 1);
     setMoveRange(ofVec3f(600,600,0));
 }
 
